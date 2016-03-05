5b953d58038d7f35596468569649505e0ef6607b
Đuro Mandinić <djuro.mandinic@gmail.com>
2016-03-05 23:08:02 +0000

    Adding service.

---
+++ src/SocialyticsBundle/Service/TwitterService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SocialyticsBundle\Service;
+
+/**
+ * Description of TwitterService
+ *
+ * @author djuro
+ */
+class TwitterService
+{
+    //curl --get 'https://api.twitter.com/1.1/followers/ids.json' --data 'count=100&cursor=-1&screen_name=dmandini' --header 'Authorization: OAuth oauth_consumer_key="WjdCnrFSf9Odp23p6AJNLgeuI", oauth_nonce="d5c48a2a9fb18bd73c3ab54ca3d620fd", oauth_signature="jBQ7PuJSYGuOIQGiFkf6a9pdP4U%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1457218251", oauth_token="55793305-T5DxxlRcLHkqLR7AJieBCuE5u1ynnQmXHjb9Tlyhj", oauth_version="1.0"' --verbose
+}
