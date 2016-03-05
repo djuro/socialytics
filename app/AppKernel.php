fede88d88c81f701032cc9e90bd0d055c0670a2a
Đuro Mandinić <djuro.mandinic@gmail.com>
2016-03-05 21:16:03 +0000

    Fixing deploy.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,13 +18,15 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new SocialyticsBundle\SocialyticsBundle(),
             new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
+            new Symfony\Bundle\DebugBundle\DebugBundle(),
+            new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle(),
+            new Sensio\Bundle\DistributionBundle\SensioDistributionBundle(),
+            new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
-            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            
+            //$bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
 
         return $bundles;
