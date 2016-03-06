346c4a6fc832fc1c5b7d52b24609ea0e28a4f3de
Đuro Mandinić <djuro.mandinic@gmail.com>
2016-03-06 13:13:45 +0000

    Decoding json.

--- src/SocialyticsBundle/Service/TwitterService.php
+++ src/SocialyticsBundle/Service/TwitterService.php
@@ -58,9 +58,11 @@ class TwitterService
         $requestMethod = 'GET';
 
         $twitter = new TwitterAPIExchange($settings);
-        echo $twitter->setGetfield($getfield)
+        $jsonResponse = $twitter->setGetfield($getfield)
         ->buildOauth($url, $requestMethod)
         ->performRequest();
+        
+        var_dump(json_decode($jsonResponse));
     }
     //curl --get 'https://api.twitter.com/1.1/followers/ids.json' --data 'count=100&cursor=-1&screen_name=dmandini' --header 'Authorization: OAuth oauth_consumer_key="WjdCnrFSf9Odp23p6AJNLgeuI", oauth_nonce="d5c48a2a9fb18bd73c3ab54ca3d620fd", oauth_signature="jBQ7PuJSYGuOIQGiFkf6a9pdP4U%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1457218251", oauth_token="55793305-T5DxxlRcLHkqLR7AJieBCuE5u1ynnQmXHjb9Tlyhj", oauth_version="1.0"' --verbose
 
